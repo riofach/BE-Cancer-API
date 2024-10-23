f0d904c88f6101c0fd9e92d41f1c9bbf518a2a90
Louis Michael <2111500071@mahasiswa.atmaluhur.ac.id>
2024-10-24 00:45:31 +0700

    first commit

---
+++ src/services/loadModel.js
@@ -0,0 +1,14 @@
+const tf = require('@tensorflow/tfjs-node');
+
+async function loadModel() {
+    try {
+        const model = await tf.loadGraphModel(process.env.MODEL_URL);
+        console.log('Model successfully loaded');
+        return model;
+    } catch (error) {
+        console.error('Failed to load model:', error);
+        throw new Error('Model could not be loaded');
+    }
+}
+
+module.exports = loadModel;
